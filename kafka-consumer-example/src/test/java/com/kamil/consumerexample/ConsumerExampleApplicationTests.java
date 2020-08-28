bee8c25890d7013219dd55cc297101555bf5459e
Aydin <kamil.aydin@sogeti.com>
2020-08-28 10:08:18 +0200

    WORK IN PROGRESS

---
+++ kafka-consumer-example/src/test/java/com/kamil/consumerexample/ConsumerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.consumerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ kafka-producer-example/src/test/java/com/kamil/producerexample/ProducerControllerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.producerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerControllerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
